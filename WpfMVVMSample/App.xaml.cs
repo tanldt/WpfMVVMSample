515c25059ff9f314b34a24c48620822f9cbc591e
Tan Le <letanphp@gmail.com>
2021-05-07 11:41:33 -0600

    Add project files.

---
+++ WpfMVVMSample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMVVMSample.ViewModel;
+
+namespace WpfMVVMSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            WpfMVVMSample.MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
